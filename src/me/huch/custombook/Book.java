8e22b5038e78115bc192095a0062d86cd3b9a7a8
DuleHUCH <dusanzivkovicprivate@gmail.com>
2018-01-13 20:54:17 +0100

    First Commit

---
+++ src/me/huch/custombook/Book.java
@@ -0,0 +1,68 @@
+package me.huch.custombook;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftMetaBook;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.List;
+
+public class Book {
+    private Core instance;
+
+    Book(Core plugin) {
+        this.instance = plugin;
+    }
+
+    public ItemStack build() {
+        try {
+            ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+            BookMeta meta = (BookMeta) book.getItemMeta();
+            List<IChatBaseComponent> pages = (List<IChatBaseComponent>) CraftMetaBook.class.getDeclaredField("pages").get(meta);
+
+            int pageCount = instance.getConfig().getInt("pages-count");
+            for (int i = 1; i <= pageCount; i++) {
+                String pageS = "page" + i;
+                int lineCount = instance.getConfig().getInt(pageS + ".line-count");
+                TextComponent base = new TextComponent();
+                for (int b = 1; b <= lineCount; b++) {
+                    String lineText = instance.getConfig().getString(pageS + ".line" + b + ".text");
+                    TextComponent textComponent = new TextComponent(lineText);
+                    if (instance.getConfig().getBoolean(pageS + ".line" + b + ".clickable")) {
+                        String clickAction = instance.getConfig().getString(pageS + ".line" + b + ".click-action");
+                        String clickActionText = instance.getConfig().getString(pageS + ".line" + b + ".click-action-text");
+                        textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.valueOf(clickAction), clickActionText));
+                    }
+                    if (instance.getConfig().getBoolean(pageS + ".line" + b + ".hoverover")) {
+                        String hoverText = instance.getConfig().getString(pageS + ".line" + b + ".hoverover-text");
+                        textComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hoverText).create()));
+                    }
+                    if (instance.getConfig().getBoolean(pageS + ".line" + b + ".text-bold")) {
+                        textComponent.setBold(true);
+                    }
+                    net.md_5.bungee.api.ChatColor color = net.md_5.bungee.api.ChatColor.valueOf(instance.getConfig().getString(pageS + ".line" + b + ".text-color"));
+                    textComponent.setColor(color);
+
+                    base.addExtra(textComponent);
+                }
+
+                IChatBaseComponent page = IChatBaseComponent.ChatSerializer.a(ComponentSerializer.toString(base));
+                pages.add(page);
+            }
+
+            meta.setTitle("Made by Huch w help of other people from Spigot");
+            book.setItemMeta(meta);
+
+            return book;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/me/huch/custombook/Core.java
@@ -0,0 +1,61 @@
+package me.huch.custombook;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.server.v1_12_R1.PacketDataSerializer;
+import net.minecraft.server.v1_12_R1.PacketPlayOutCustomPayload;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Core extends JavaPlugin implements Listener {
+    private Book customBook;
+
+    @Override
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        this.customBook = new Book(this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player user = event.getPlayer();
+
+        delayedTask(user);
+    }
+
+    private void delayedTask(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                sendPacket(customBook.build(), player);
+            }
+        }.runTaskLater(this, 5);
+    }
+
+    private void sendPacket(ItemStack book, Player player) {
+        int slot = player.getInventory().getHeldItemSlot();
+        ItemStack old = player.getInventory().getItem(slot);
+        player.getInventory().setItem(slot, book);
+
+        ByteBuf buf = Unpooled.buffer(256);
+        buf.setByte(0, (byte)0);
+        buf.writerIndex(1);
+
+        PacketPlayOutCustomPayload packet = new PacketPlayOutCustomPayload("MC|BOpen", new PacketDataSerializer(buf));
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+
+        player.getInventory().setItem(slot, old);
+    }
+
+}
